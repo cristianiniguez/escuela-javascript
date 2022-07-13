6a92f177cd55726b8416d8ff6f630e015cc6dd9d
Cristian Iñiguez <felizpc1@gmail.com>
2022-07-12 20:40:44 -0400

    paginas, rutas y componentes: header en todas las rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import Header from '../components/Header';
+
 const Home = () => {
-  return <div>Platzi YardSale</div>;
+  return <Header />;
 };
 
 export default Home;
