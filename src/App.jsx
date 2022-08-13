3d6e8599f933f6ef5265e1a89dd2a90c8e22b4ba
cristianiniguez <felizpc1@gmail.com>
2022-08-12 21:58:03 -0400

    logica con react hooks: react context

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,29 @@ import SignUp from '@pages/SignUp';
 import Checkout from '@pages/Checkout';
 import MyOrder from '@pages/MyOrder';
 import NotFound from '@pages/NotFound';
+import AppContext from '@context/AppContext';
+import useInitialState from '@hooks/useInitialState';
 
 const App = () => {
+  const initialState = useInitialState();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/reset-password' element={<ResetPassword />} />
-        <Route path='/email-sent' element={<EmailSent />} />
-        <Route path='/new-password' element={<NewPassword />} />
-        <Route path='/account' element={<Account />} />
-        <Route path='/checkout' element={<Checkout />} />
-        <Route path='/my-order' element={<MyOrder />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/email-sent' element={<EmailSent />} />
+          <Route path='/new-password' element={<NewPassword />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/my-order' element={<MyOrder />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import ShoppingCartIcon from './ShoppingCartIcon';
+import AppContext from '@context/AppContext';
 
 const Product = ({ product }) => {
+  const { addToCart } = useContext(AppContext);
+
   return (
     <div>
       <img
@@ -16,7 +19,7 @@ const Product = ({ product }) => {
           <p className='font-bold mb-1'>${product.price}</p>
           <p className='text-sm text-gray-500'>{product.title}</p>
         </div>
-        <div className='rounded-full bg-blue-300 p-1' onClick={() => null}>
+        <div className='rounded-full bg-blue-300 p-1' onClick={() => addToCart(product)}>
           <ShoppingCartIcon className='text-white' point='+' />
         </div>
       </div>
