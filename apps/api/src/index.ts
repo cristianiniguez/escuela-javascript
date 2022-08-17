05e53d8f565e7a69fd6eed26a844d5fb6084006a
cristianiniguez <felizpc1@gmail.com>
2022-08-16 20:26:05 -0400

    introduccion: instalacion de express.js y tu primer servidor http

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,12 @@
-console.log('My App');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
