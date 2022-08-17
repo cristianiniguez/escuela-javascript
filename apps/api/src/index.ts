ab3df625ccbbaaaacdaa1c543faccfd045b5ef38
cristianiniguez <felizpc1@gmail.com>
2022-08-16 20:31:26 -0400

    introduccion: routing con express.js

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
