9e79fcf9e4b3d942f0cb82db09006c34fa40f863
cristianiniguez <felizpc1@gmail.com>
2022-09-11 16:15:25 -0400

    deployment - problema de cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import apiRouter from './routes';
 import { boomErrorHandler, errorHandler, logErrors } from './middlewares/error.handler';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
