4b63b9b3d5ec7ff55677da5856baf5a7f4444cea
cristianiniguez <felizpc1@gmail.com>
2022-11-05 15:12:08 -0400

    despliegue - deploy en ...

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -11,12 +11,26 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
   db: {
+    url: getConfig('DATABASE_URL'),
     user: getConfig('DB_USER'),
     password: getConfig('DB_PASSWORD'),
     host: getConfig('DB_HOST'),
     name: getConfig('DB_NAME'),
     port: getConfig('DB_PORT'),
   },
+};
+
+export const isProd = () => config.env === 'production';
+
+export const getDbURL = () => {
+  const { host, name, port, url } = config.db;
+
+  // in production, the url is directly provided
+  if (isProd()) return url;
+
+  const user = encodeURIComponent(config.db.user);
+  const password = encodeURIComponent(config.db.password);
+  return `postgres://${user}:${password}@${host}:${port}/${name}`;
 };
 
 export default config;
--- apps/api/src/libs/sequelize.ts
+++ apps/api/src/libs/sequelize.ts
@@ -1,13 +1,12 @@
-import { Sequelize } from 'sequelize';
-import config from '../config';
+import { Options, Sequelize } from 'sequelize';
+import { getDbURL, isProd } from '../config';
 import setupModels from '../db/models';
 
-const { host, name, port } = config.db;
-const user = encodeURIComponent(config.db.user);
-const password = encodeURIComponent(config.db.password);
-const uri = `postgres://${user}:${password}@${host}:${port}/${name}`;
+const options: Options = { dialect: 'postgres', logging: !isProd() };
 
-const sequelize = new Sequelize(uri, { dialect: 'postgres', logging: true });
+if (isProd()) options.dialectOptions = { ssl: { rejectUnauthorized: false } };
+
+const sequelize = new Sequelize(getDbURL(), options);
 
 export const setupDb = () => setupModels(sequelize);
 
