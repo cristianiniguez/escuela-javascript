1616c72124d8351e9fc41ff3b9c3e92f0fde252a
cristianiniguez <felizpc1@gmail.com>
2022-11-05 15:53:59 -0400

    despliegue - consideraciones al hacer migraciones

--- apps/api/src/db/migrations/2022.10.17T04.13.02.create-table-user.ts
+++ apps/api/src/db/migrations/2022.10.17T04.13.02.create-table-user.ts
@@ -1,9 +1,10 @@
 import { QueryInterface } from 'sequelize';
 import { MigrationFn } from 'umzug';
+import _ from 'lodash';
 import { UserSchema, USER_TABLE } from '../models/user.model';
 
 export const up: MigrationFn<QueryInterface> = async ({ context: queryInterface }) => {
-  await queryInterface.createTable(USER_TABLE, UserSchema);
+  await queryInterface.createTable(USER_TABLE, _.omit(UserSchema, ['role']));
 };
 
 export const down: MigrationFn<QueryInterface> = async ({ context: queryInterface }) => {
