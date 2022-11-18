54ce1774a691ad01401371bfcb6918ae28716608
cristianiniguez <felizpc1@gmail.com>
2022-11-17 21:06:27 -0400

    passport y json web tokens - firmar y verificar tokens

--- apps/api/src/utils/index.ts
+++ apps/api/src/utils/index.ts
@@ -1,5 +1,14 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = '123abc'; // TODO: move this to an env var
+
+export type JWTPayload = { sub: number; role: string };
 
 export const hashPassword = (password: string) => bcrypt.hash(password, 10);
 
 export const verifyPassword = (password: string, hash: string) => bcrypt.compare(password, hash);
+
+export const signToken = (payload: JWTPayload) => jwt.sign(payload, SECRET);
+
+export const verifyToken = (token: string) => jwt.verify(token, SECRET);
