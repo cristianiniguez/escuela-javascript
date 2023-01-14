87506f0385b5740f9d5084f78011f5aae52bc3f3
cristianiniguez <felizpc1@gmail.com>
2023-01-14 14:05:18 -0400

    tipado en graphql - sistema de tipado

--- apps/api/src/graphql/index.ts
+++ apps/api/src/graphql/index.ts
@@ -1,16 +1,22 @@
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { GraphQLFieldResolver as Resolver } from 'graphql';
 import { Express } from 'express';
 
 const typeDefs = `
   type Query {
-    hello: String
+    hello: String!
+    getPerson(name: String!, age: Int!): String!
   }
 `;
+
+const getPerson: Resolver<unknown, unknown, { name: string; age: number }, string> = (_, args) =>
+  `Hello, my name is ${args.name}, and I'm ${args.age} year(s) old.`;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello World',
+    getPerson,
   },
 };
 
