e03e66e0f1932fef028085a5731df64fd5cbadfc
cristianiniguez <felizpc1@gmail.com>
2023-01-21 22:33:00 -0400

    conectado a ProductsService

--- apps/api/src/graphql/product.resolvers.ts
+++ apps/api/src/graphql/product.resolvers.ts
@@ -1,7 +1,11 @@
 import { GraphQLFieldResolver as Resolver } from 'graphql';
+import ProductsService from '../services/products.service';
 
-export const getProducts: Resolver<unknown, unknown> = () => [];
+const productsService = new ProductsService();
 
-export const getProduct: Resolver<unknown, unknown, { id: string }> = (_, { id }) => ({});
+export const getProducts: Resolver<unknown, unknown> = () => productsService.find({});
+
+export const getProduct: Resolver<unknown, unknown, { id: string }> = (_, { id }) =>
+  productsService.findOne(+id);
 
 export const addProduct: Resolver<unknown, unknown> = () => ({});
