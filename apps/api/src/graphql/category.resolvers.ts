202d8af30874a243929326d7d3fbe708b677e24b
cristianiniguez <felizpc1@gmail.com>
2023-02-05 13:25:15 -0400

    autenticacion: graphql con jwt

--- apps/api/src/graphql/category.resolvers.ts
+++ apps/api/src/graphql/category.resolvers.ts
@@ -1,9 +1,17 @@
+import boom from '@hapi/boom';
 import { GraphQLFieldResolver as Resolver } from 'graphql';
+import { Context } from 'graphql-passport/lib/buildContext';
 import CategoriesService, { CreateCategoryDTO } from '../services/categories.service';
 
 const categoriesService = new CategoriesService();
 
-export const addCategory: Resolver<unknown, unknown, { input: CreateCategoryDTO }> = (
-  _,
-  { input },
-) => categoriesService.create(input);
+export const addCategory: Resolver<
+  unknown,
+  Context<Express.User>,
+  { input: CreateCategoryDTO }
+> = async (_, { input }, context) => {
+  const { user } = await context.authenticate('jwt', { session: false });
+  if (!user) throw boom.unauthorized('invalid jwt');
+
+  return categoriesService.create(input);
+};
